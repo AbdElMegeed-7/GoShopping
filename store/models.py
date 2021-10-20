b7c2cd0990481ac74c6ee643d40e1c40f900727c
AbdElMegeed-7 <AbdElMegeedX7@gmail.com>
2021-10-20 03:30:29 +0200

    Increment, decrement and remove Cart Items

--- store/models.py
+++ store/models.py
@@ -2,20 +2,21 @@ from django.db import models
 from django.urls import reverse
 from category.models import Category
 
+
 class Product(models.Model):
-    product_name    = models.CharField(max_length=255, unique=True)
-    slug            = models.CharField(max_length=255, unique=True)
-    description     = models.TextField(max_length=255)
-    price           = models.IntegerField()
-    stock           = models.IntegerField()
-    image           = models.ImageField(upload_to='images/products')
-    is_available    = models.BooleanField(default=True)
-    category        = models.ForeignKey(Category, on_delete=models.CASCADE)
-    created_date    = models.DateTimeField(auto_now_add=True)
-    modified_date   = models.DateTimeField(auto_now=True)
-    
+    product_name = models.CharField(max_length=255, unique=True)
+    slug = models.CharField(max_length=255, unique=True)
+    description = models.TextField(max_length=255)
+    price = models.IntegerField()
+    stock = models.IntegerField()
+    image = models.ImageField(upload_to='images/products')
+    is_available = models.BooleanField(default=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    created_date = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now=True)
+
     def __str__(self):
         return self.product_name
-    
+
     def get_url(self):
         return reverse('product_detail', args=[self.category.slug, self.slug])
